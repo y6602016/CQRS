cf7f7e9ed1c4b6802c21b032ce8aa25d4c92bad0
Mike Hong <y6602016@gmail.com>
2023-06-11 17:21:27 -0400

    add aggregate root

--- CQRS-ES/CQRS.Core/Infrastructure/ICommandDispatcher.cs
+++ CQRS-ES/CQRS.Core/Infrastructure/ICommandDispatcher.cs
@@ -2,7 +2,7 @@ using CQRS.Core.Commands;
 
 namespace CQRS.Core.Infrastructure
 {
-  // dispatcher working as a mediator for command handlers
+  // dispatcher working as a mediator that manages the distribution of messages(commands) among other objects
   public interface ICommandDispatcher
   {
     void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand;
